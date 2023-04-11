b47a0ec99b4c47828299f067b2e2aa281db9746d
Charles T <chalodss@proton.me>
2023-04-11 14:27:59 +0200

    Create stack_list.h

---
+++ C-Learning/DataStructures/Stack/stack_list.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright(C) 2023, Charles T, <chalodss@proton.me> 
+ *
+ */
+
+#ifndef _STACK_LIST_H_
+#define _STACK_LIST_H_
+
+typedef struct    s_stack
+{
+  void            *data;
+  struct s_stack  *next;
+}                 Stack;
+
+#endif
