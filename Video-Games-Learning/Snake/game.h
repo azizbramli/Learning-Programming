84e109ffd7766802199544b46f49d32f0908851e
Charles T <chalodss@proton.me>
2023-04-11 21:49:32 +0200

    Create game.h

---
+++ Video-Games-Learning/Snake/game.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright(C) 2023, Charles T, <chalodss@proton.me> 
+ *
+ */
+
+#ifndef _GAME_H_
+#define _GAME_H_
+
+#include <ncurses.h>
+
+#define ROWS        30
+#define COLUMNS     80
+#define SIZE_SNAKE  256
+
+typedef enum  Direction {NORTH, EAST, SOUTH, WEST}  Direction;
+
+typedef struct  {
+  int x;
+  int y;
+}     Entity;
+
+typedef struct {
+  bool      life;
+  int       right_limit;
+  int       bottom_limit;
+  int       length;
+  Entity    food;
+  Entity    snake[SIZE_SNAKE];
+  Entity    tail;
+  Direction direction;
+}           Game;
+
+void    game(Game *, int, int, int);
+void    run(Game *, WINDOW *, long);
+
+#endif
