d2fad9274735990a07951d37da6ac7096b3c4c81
Charles T <chalodss@proton.me>
2023-04-11 15:56:40 +0200

    Update stack_list.h

--- C-Learning/DataStructures/Stack/stack_list.h
+++ C-Learning/DataStructures/Stack/stack_list.h
@@ -6,10 +6,16 @@
 #ifndef _STACK_LIST_H_
 #define _STACK_LIST_H_
 
+#include <stdbool.h>
+
 typedef struct    s_stack
 {
   void            *data;
   struct s_stack  *next;
 }                 Stack;
+
+bool    is_empty(Stack *);
+void    push(Stack **, Stack *);
+Stack   *pop(Stack **);
 
 #endif
