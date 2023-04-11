31f710e94a3987826b183bb78e6bf8bf9284d107
Charles T <chalodss@proton.me>
2023-04-11 18:24:27 +0200

    Create data.h

---
+++ C-Learning/DataStructures/HashTables/data.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright(C) 2023, Charles T, <chalodss@proton.me> 
+ *
+ */
+
+#ifndef _DATA_H_
+#define _DATA_H_
+
+#include "structs.h"
+
+void    get_data(char *, union Player *, int);
+
+#endif
