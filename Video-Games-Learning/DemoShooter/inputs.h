47c25b01977a91b55a9ddf7afc2aa7efdd4af3f0
Charles T <chalodss@proton.me>
2023-04-11 22:00:50 +0200

    Create inputs.h

---
+++ Video-Games-Learning/DemoShooter/inputs.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright(C) 2023, Charles T, <chalodss@proton.me> 
+ *
+ */
+
+
+#ifndef _INPUTS_H_
+#define _INPUTS_H_
+
+#include "structs.h"
+
+void    get_inputs(SDL_Event *, Game *);
+
+#endif
