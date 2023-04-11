abd0e45387e9de98d077491d16adaab2bd9a4322
Charles T <chalodss@proton.me>
2023-04-11 14:29:00 +0200

    Create stack_list.c

---
+++ C-Learning/DataStructures/Stack/stack_list.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright(C) 2023, Charles T, <chalodss@proton.me> 
+ *
+ */
+
+#include <stdbool.h>
+#include <stddef.h>
+#include "stack_list.h"
+
+bool    is_empty(Stack *stack)
+{
+  return stack == NULL;
+}
+
+void    push(Stack **stack, Stack *elem)
+{
+  elem->next = *stack;
+  *stack = elem;
+}
+
+Stack   *pop(Stack **stack)
+{
+  Stack *elem = *stack;
+
+  *stack = elem->next;
+  return elem;  
+}
