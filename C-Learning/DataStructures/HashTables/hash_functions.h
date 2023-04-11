e6a4e4e80c79f9721ed732369ebf82ecaf283217
Charles T <chalodss@proton.me>
2023-04-11 16:14:27 +0200

    Create hash_functions.h

---
+++ C-Learning/DataStructures/HashTables/hash_functions.h
@@ -0,0 +1,11 @@
+/*
+ * Copyright(C) 2023, Charles T, <chalodss@proton.me> 
+ *
+ */
+
+#ifndef _HASH_FUNCTIONS_H_
+#define _HASH_FUNCTIONS_H_
+
+unsigned int    elf_hash(const char*, unsigned int);
+
+#endif
