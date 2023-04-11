841cc9f6f1c8497a537c842452ebbfdb56ffd41d
Charles T <chalodss@proton.me>
2023-04-11 21:34:45 +0200

    Create constants.h

---
+++ C-Learning/DataStructures/Trees/constants.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright(C) 2023, Charles T, <chalodss@proton.me> 
+ *
+ */
+
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
+
+#define RED         "\033[1;31m"
+#define TGREEN      "\033[2;33m"
+#define BLUE        "\033[1;34m"
+#define PINK        "\033[1;35m"
+#define TCYAN       "\033[2;36m"
+#define EMPTY       "\033[0m"
+
+#endif
